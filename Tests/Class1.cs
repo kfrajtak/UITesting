6af68e79d5002aabec92db71f81f704cae859332
Karel Frajtak <kfrajtak@gmail.com>
2016-12-02 18:05:56 +0100

    1st commit

---
+++ Tests/Class1.cs
@@ -0,0 +1,68 @@
+﻿using OpenQA.Selenium.Appium.iOS;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Class1
+    {
+        protected const string AppDriverUrl = "http://127.0.0.1:4723";
+        protected const string AppId = @"C:\Work\playground\dist\App\bin\Debug\App.exe";
+
+        [Fact]
+        public void Test1()
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("appId", "App");
+            appCapabilities.SetCapability("app", AppId);
+            var session = new RemoteWebDriver(new Uri(AppDriverUrl), appCapabilities);
+            session.FindElementById("textBox1").SendKeys("This is some text");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("appId", "App");
+            appCapabilities.SetCapability("app", AppId);
+            var session = new RemoteWebDriver(new Uri(AppDriverUrl), appCapabilities);
+            session.FindElementByName("textBox1").SendKeys("This is some text");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("appId", "App");
+            appCapabilities.SetCapability("app", AppId);
+            var session = new IOSDriver<IOSElement>(new Uri(AppDriverUrl), appCapabilities);
+            session.FindElementById("textBox1").SendKeys("This is some text");
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("appId", "App");
+            appCapabilities.SetCapability("app", AppId);
+            var session = new IOSDriver<IOSElement>(new Uri(AppDriverUrl), appCapabilities);
+            session.FindElementByName("textBox1").SendKeys("This is some text");
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("appId", "App");
+            appCapabilities.SetCapability("app", AppId);
+            var session = new RemoteWebDriver(new Uri(AppDriverUrl), appCapabilities);
+            session.FindElementByTagName("Edit").SendKeys("This is some text");
+            session.Quit();
+        }
+    }
+}
---
+++ Tests/Class2.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium.Appium.iOS;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Class2
+    {
+        protected const string AppDriverUrl = "http://127.0.0.1:4723";
+        protected const string WpfNotepadAppId = @"C:\Users\Karel\Downloads\wpfnotepad\Demo - WPF Notepad\Demo - WPF Notepad\bin\Debug\Demo - WPF Notepad.exe"; //copy and build binary from source https://codeoverload.wordpress.com/2010/05/01/wpf-notepad/   
+        protected const string ExplorerAppId = @"wpfnotepad.exe";
+        protected const string TargetSaveLocation = @"%TEMP%\";
+
+        [Fact]
+        public void BasicScenario()
+        {
+            // Launch Word Classic Windows Application
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", WpfNotepadAppId);
+            var WpfnotepadSession = new IOSDriver<IOSElement>(new Uri(AppDriverUrl), appCapabilities);
+            Assert.NotNull(WpfnotepadSession);
+
+            var TextValue = "Hello WPF Notepad Application";
+            WpfnotepadSession.FindElementByName("File").Click();
+            WpfnotepadSession.FindElementByName("New").Click();
+            var editBox = WpfnotepadSession.FindElementByClassName("TextBox");
+            editBox.SendKeys(TextValue);
+            Assert.Equal(TextValue, editBox.Text);
+            System.Threading.Thread.Sleep(4000);
+            WpfnotepadSession.Quit();
+        }
+
+        [Fact]
+        public void BasicScenario2()
+        {
+            // Launch Word Classic Windows Application
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", WpfNotepadAppId);
+            var WpfnotepadSession = new RemoteWebDriver(new Uri(AppDriverUrl), appCapabilities);
+            Assert.NotNull(WpfnotepadSession);
+
+            var TextValue = "Hello WPF Notepad Application";
+            WpfnotepadSession.FindElementByName("File").Click();
+            WpfnotepadSession.FindElementByName("New").Click();
+            var editBox = WpfnotepadSession.FindElementByClassName("TextBox");
+            editBox.SendKeys(TextValue);
+            Assert.Equal(TextValue, editBox.Text);
+            System.Threading.Thread.Sleep(4000);
+            WpfnotepadSession.Quit();
+        }
+    }
+}
